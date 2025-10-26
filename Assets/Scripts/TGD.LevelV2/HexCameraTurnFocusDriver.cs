596a77179e0e6f3600f5f42ebe67073eb6e16f20
ReinenVernunft <janeray1234@gmail.com>
2025-10-27 00:51:04 +0800

    Add camera recenter controls and auto focus on active unit

---
+++ Assets/Scripts/TGD.LevelV2/HexCameraTurnFocusDriver.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using TGD.CombatV2;
+using TGD.HexBoard;
+
+namespace TGD.LevelV2
+{
+    /// <summary>
+    /// 自动将 HexCameraControllerHB 对准当前行动单位。
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class HexCameraTurnFocusDriver : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] HexCameraControllerHB cameraController;
+        [SerializeField] TurnManagerV2 turnManager;
+        [SerializeField] CombatActionManagerV2 combatManager;
+        [SerializeField] bool focusOnNullChainFallbackToActive = true;
+
+        Unit _lastFocusedUnit;
+
+        static T AutoFind<T>() where T : Object
+        {
+#if UNITY_2023_1_OR_NEWER
+            return Object.FindFirstObjectByType<T>(FindObjectsInactive.Include);
+#else
+            return Object.FindObjectOfType<T>();
+#endif
+        }
+
+        void Awake()
+        {
+            if (!cameraController)
+                cameraController = GetComponent<HexCameraControllerHB>();
+            if (!turnManager)
+                turnManager = AutoFind<TurnManagerV2>();
+            if (!combatManager)
+                combatManager = AutoFind<CombatActionManagerV2>();
+        }
+
+        void OnEnable()
+        {
+            Subscribe();
+            TryFocus(turnManager != null ? turnManager.ActiveUnit : null);
+        }
+
+        void OnDisable()
+        {
+            Unsubscribe();
+        }
+
+        void Subscribe()
+        {
+            if (turnManager != null)
+                turnManager.TurnStarted += OnTurnStarted;
+            if (combatManager != null)
+                combatManager.ChainFocusChanged += OnChainFocusChanged;
+        }
+
+        void Unsubscribe()
+        {
+            if (turnManager != null)
+                turnManager.TurnStarted -= OnTurnStarted;
+            if (combatManager != null)
+                combatManager.ChainFocusChanged -= OnChainFocusChanged;
+        }
+
+        void OnTurnStarted(Unit unit)
+        {
+            TryFocus(unit);
+        }
+
+        void OnChainFocusChanged(Unit unit)
+        {
+            if (unit == null && focusOnNullChainFallbackToActive && turnManager != null)
+            {
+                TryFocus(turnManager.ActiveUnit);
+                return;
+            }
+
+            TryFocus(unit);
+        }
+
+        void TryFocus(Unit unit)
+        {
+            if (cameraController == null || unit == null)
+                return;
+
+            if (_lastFocusedUnit == unit && cameraController.GetFocusCoordinate().Equals(unit.Position))
+                return;
+
+            cameraController.AutoFocus(unit.Position);
+            _lastFocusedUnit = unit;
+        }
+    }
+}
+
